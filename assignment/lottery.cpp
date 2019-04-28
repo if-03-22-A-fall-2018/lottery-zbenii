0f15330745120bf4e30d27fb5edf8e972c875e0d
zbenii <besic.beni@gmx.at>
2019-04-28 22:21:36 +0200

    something done

--- assignment/lottery.cpp
+++ assignment/lottery.cpp
@@ -12,32 +12,81 @@
 */
 #include <stdio.h>
 #include "lottery.h"
+#include <string.h>
+#include <stdlib.h>
 
 #define UUID_LEN 40
 #define MAX_TIP_LEN 17
 #define MAX_LINE_LEN (UUID_LEN + 1 + MAX_TIP_LEN + 1)
 
-char sep;
-char* file;
+static FILE* stream;
+static char separator;
+static int drawing_of_numbers[TIP_SIZE];
 
 bool 	init_lottery (const char *csv_file, char csv_separator){
-  FILE* fd=fopen(csv_file,"r");
-  if(fd==0)
+  stream = fopen(csv_file, "r");
+  if (stream == 0)
   {
     return false;
   }
-  sep = csv_separator;
-  file=csv_file;
-  fclose(fd);
+  separator = csv_separator;
+
+  for (int i = 0; i < TIP_SIZE; ++i)
+  {
+    drawing_of_numbers[i] = 0;
+  }
   return true;
 }
 
 bool 	get_tip (int tip_number, int tip[TIP_SIZE]){
-  return true;
+  if (tip_number < 0 || tip_number >= 44)
+  {
+    return false;
+  }
+
+  int count = 0;
+  char line[MAX_LINE_LEN];
+  while ( fgets(line, MAX_LINE_LEN, stream) != NULL)
+  {
+      if (count == tip_number)
+      {
+          count = 0;
+          char str[2];
+          str[0] = separator;
+          str[1] = '\0';
+          char* ptr = strtok(line, str);
+          while(ptr != 0)
+          {
+              ptr = strtok(0, str);
+              if (atoi(ptr) > 0 && atoi(ptr) <= 45)
+              {
+                  tip[count] = atoi(ptr);
+              }
+              count++;
+          }
+          rewind(stream);
+          return true;
+      }
+      count++;
+  }
+  return false;
 }
+
 bool 	set_drawing (int drawing_numbers[TIP_SIZE]){
+  for (int i = 0; i < TIP_SIZE; i++)
+  {
+    if (drawing_numbers[i] <= 0 || drawing_numbers[i] > 45)
+    {
+      return false;
+    }
+    else
+    {
+      drawing_of_numbers[i] = drawing_numbers[i];
+    }
+  }
   return true;
 }
+
 int 	get_tip_result (int tip_number){
   return 0;
 }
